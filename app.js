8dd2777e4e1884a04b859a31065a8cafdc367349
kobyjackcheese <kwrape@icloud.com>
2023-02-27 16:28:02 -0600

    koby project one

---
+++ app.js
@@ -0,0 +1,27 @@
+const rockButton = document.querySelector('.rock');
+//console.log(rockButton);
+const paperButton = document.querySelector('.paper');
+//console.log(paperButton);
+const scissorsButton = document.querySelector('.scissors');
+//console.log(scissorsButton);
+const playerScore = document.querySelector('.playerScore');
+//console.log(playerScore);
+const cpuScore = document.querySelector('.cpuScore');
+//console.log(cpuScore);
+
+function pickRock() {
+console.log('wow rock')
+}
+
+function pickPaper() {
+    console.log('wow paper')
+}
+
+function pickScissors() {
+console.log('wow scissors')
+}
+
+
+rockButton.addEventListener('click', pickRock);
+paperButton.addEventListener('click', pickPaper);
+scissorsButton.addEventListener('click', pickScissors);
---
+++ readme.md
@@ -0,0 +1,7 @@
+Project One:
+
+Rock Paper Scissors
+
+this will be a game where you play rock paper scissors against a computer. perhaps when i am finished with the basics of the project i might make the computer make choices based on the player's past selections.
+
+i will try and make a section of the page that shows the past selections of both the player and the computer.
