73fb88b611889aac3bf544df2ca6c78dd34a25c9
kobyjackcheese <kwrape@icloud.com>
2023-02-28 18:29:00 -0600

    html and css mostly done and readme updated

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const rockButton = document.querySelector('.rock');
-//console.log(rockButton);
-const paperButton = document.querySelector('.paper');
-//console.log(paperButton);
-const scissorsButton = document.querySelector('.scissors');
-//console.log(scissorsButton);
-const playerScore = document.querySelector('.playerScore');
-//console.log(playerScore);
-const cpuScore = document.querySelector('.cpuScore');
-//console.log(cpuScore);
+// const rockButton = document.querySelector('.rock');
+// //console.log(rockButton);
+// const paperButton = document.querySelector('.paper');
+// //console.log(paperButton);
+// const scissorsButton = document.querySelector('.scissors');
+// //console.log(scissorsButton);
+// const playerScore = document.querySelector('.playerScore');
+// //console.log(playerScore);
+// const cpuScore = document.querySelector('.cpuScore');
+// //console.log(cpuScore);
+
+let selections = ['rock', 'paper', 'scissors'];
 
 function pickRock() {
 console.log('wow rock')
@@ -20,8 +22,7 @@ function pickPaper() {
 function pickScissors() {
 console.log('wow scissors')
 }
-
-
+console.log(selections);
 rockButton.addEventListener('click', pickRock);
 paperButton.addEventListener('click', pickPaper);
 scissorsButton.addEventListener('click', pickScissors);
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ Rock Paper Scissors
 this will be a game where you play rock paper scissors against a computer. perhaps when i am finished with the basics of the project i might make the computer make choices based on the player's past selections.
 
 i will try and make a section of the page that shows the past selections of both the player and the computer.
+
+player should be able to click a button, making a selection which then triggers the computer to make its own selection (maybe based on the past 5 or so selections) and then displaying the results and who wins.
